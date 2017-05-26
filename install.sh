fce6bb8b55d96b84f218e4255c78dbd29f7a1696
Xin <monojoker@gmail.com>
2017-05-25 20:18:44 -0700

    update install.sh to auto install dependencies

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 RED="$(tput setaf 1)"
 GREEN="$(tput setaf 2)"
 YELLOW="$(tput setaf 3)"
@@ -13,24 +13,26 @@ execute () {
 
 install_dep () {
 	install_cmd="sudo apt-get install $1"
-	printf "${YELLOW}We are going to install $1 on your computer ...${NORMAL}\n"
+	echo "${YELLOW}We are going to install $1 on your computer ...${NORMAL}"
 	execute "$install_cmd"
 }
 
 check_installed () {
-	loc=$(whereis "$1")
-	if [ "${#loc}" -gt $((${#1}+1)) ]; then
-		printf "${GREEN}$1 is installed!${NORMAL}\n"
-	else
-		printf "${RED}$1 hasn't been installed!${NORMAL}\n"
-		install_dep $1
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $1| \
+        grep "install ok installed")
+    if [ "" == "$PKG_OK" ]; then
+        echo "${RED}$1 hasn't been installed!${NORMAL}"
+        sudo apt-get --force-yes --yes install $1
+        install_dep $1
+    else
+		echo "${GREEN}$1 is installed!${NORMAL}"
 	fi
 }
-#check deps first
-DEPS=(vim git exuberant-ctags silversearcher-ag nodejs npm)
+
+declare -a DEPS=("vim" "git" "exuberant-ctags" "silversearcher-ag" "nodejs" "npm")
 for dep in ${DEPS[@]}
 do
-	check_installed $dep
+    check_installed $dep
 done
 
 if [ ! -d "$HOME/.yadr" ]; then
