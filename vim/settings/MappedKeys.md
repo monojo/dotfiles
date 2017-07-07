7b1272319d3f18c31e72bbb11b11e4217620baee
Xin <monojoker@gmail.com>
2017-07-07 14:57:28 -0400

    tmux update

---
+++ vim/settings/MappedKeys.md
@@ -0,0 +1,8 @@
+# Mapped Keys
+
+------
+
+- Q : close current buffer
+
+### With leader key
+- <leader>w : search current word in bookmarked folders
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -211,3 +211,6 @@ alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
 alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+
+#tmux - solve weired color in vim
+alias tmux='tmux -2'
