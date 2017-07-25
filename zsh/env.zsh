d9369bddfb945b7747c0c0f1d09c604260965e94
Xin <monojoker@gmail.com>
2017-07-24 21:39:35 -0400

    zsh env setting

---
+++ zsh/env.zsh
@@ -0,0 +1,11 @@
+export USE_CCACHE=1
+
+PATH=~/bin:$PATH
+PATH=~/android-studio/bin:$PATH
+PATH=~/scitools/bin/linux64:$PATH
+PATH=~/Android/Sdk/platform-tools:$PATH
+PATH=~/android-studio/bin:$PATH
+PATH=~/Android/Sdk/ndk-bundle:$PATH
+PATH=~/depot_tools:$PATH
+
+alias android_studio='/home/moslab/android-studio/bin/studio.sh'
