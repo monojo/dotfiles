28e9615193c8adb2209d78639a45eb1fb0447cb9
Xin <monojoker@gmail.com>
2020-08-09 01:47:19 -0400

    color parameter and change some highlight

--- doom.d/modules/private/my-cc/config.el
+++ doom.d/modules/private/my-cc/config.el
@@ -71,6 +71,7 @@
      ))
   )
 
+
 (use-package! ccls
   :hook ((c-mode-local-vars c++-mode-local-vars objc-mode-local-vars) . +ccls|enable)
   :config
@@ -79,32 +80,58 @@
   ;; (setq ccls-sem-highlight-method 'overlay)
   (setq ccls-sem-highlight-method 'font-lock)
   (add-hook 'lsp-after-open-hook #'ccls-code-lens-mode)
+;; list of ccls-sem: method, constructor, function, staticmethod
+;; namespace, struct==class, enum, typeparameter, typealias,
+;; variable, parameter, macro, field, enummember
+;; See details in ccls-sematic-highlight.el
+  (defface my-ccls-sem-parameter-face
+    '((t :weight normal :foreground "#ffd787"))
+    :group 'ccls-sem)
+
+;; (defface my-ccls-sem-struct-face
+;;   '((t :slant italic))
+;;   :group 'ccls-sem)
+
+(defface my-ccls-sem-type-face
+  '((t :weight bold :foreground "#a4ffff"))
+  :group 'ccls-sem)
+
+;; (defface my-ccls-sem-class-field-face
+;;   '((t :slant italic))
+;;   :group 'ccls-sem)
+(defface my-ccls-sem-macro-face
+  '((t :weight extra-bold))
+  :group 'ccls-sem)
   ;; rainbow highlight, will override your theme
-  ;; (ccls-use-default-rainbow-sem-highlight)
-  ;; https://github.com/maskray/ccls/blob/master/src/config.h
-  (setq
-   ccls-initialization-options
-   `(:clang
-     (:excludeArgs
-      ;; Linux's gcc options. See ccls/wiki
-      ["-falign-jumps=1" "-falign-loops=1" "-fconserve-stack" "-fmerge-constants" "-fno-code-hoisting" "-fno-schedule-insns" "-fno-var-tracking-assignments" "-fsched-pressure"
-       "-mhard-float" "-mindirect-branch-register" "-mindirect-branch=thunk-inline" "-mpreferred-stack-boundary=2" "-mpreferred-stack-boundary=3" "-mpreferred-stack-boundary=4" "-mrecord-mcount" "-mindirect-branch=thunk-extern" "-mno-fp-ret-in-387" "-mskip-rax-setup"
-       "--param=allow-store-data-races=0" "-Wa arch/x86/kernel/macros.s" "-Wa -"]
-      :extraArgs []
-      :pathMappings ,+ccls-path-mappings)
-     :completion
-     (:include
-      (:blacklist
-       ["^/usr/(local/)?include/c\\+\\+/[0-9\\.]+/(bits|tr1|tr2|profile|ext|debug)/"
-        "^/usr/(local/)?include/c\\+\\+/v1/"
-        ]))
-     :index (:initialBlacklist ,+ccls-initial-blacklist :parametersInDeclarations :json-false :trackDependency 1)))
+  (setq ccls-sem-parameter-faces [my-ccls-sem-parameter-face])
+  ;; (setq ccls-sem-macro-faces [my-ccls-sem-macro-face])
+  (setq ccls-sem-macro-faces [font-lock-keyword-face])
+  (setq ccls-sem-type-faces [my-ccls-sem-type-face])
+  ;; (setq font-lock-type-face [my-ccls-sem-type-face])
+    ;; (ccls-use-default-rainbow-sem-highlight)
+    ;; https://github.com/maskray/ccls/blob/master/src/config.h
+    (setq
+      ccls-initialization-options
+      `(:clang
+         (:excludeArgs
+           ;; Linux's gcc options. See ccls/wiki
+           ["-falign-jumps=1" "-falign-loops=1" "-fconserve-stack" "-fmerge-constants" "-fno-code-hoisting" "-fno-schedule-insns" "-fno-var-tracking-assignments" "-fsched-pressure"
+             "-mhard-float" "-mindirect-branch-register" "-mindirect-branch=thunk-inline" "-mpreferred-stack-boundary=2" "-mpreferred-stack-boundary=3" "-mpreferred-stack-boundary=4" "-mrecord-mcount" "-mindirect-branch=thunk-extern" "-mno-fp-ret-in-387" "-mskip-rax-setup"
+             "--param=allow-store-data-races=0" "-Wa arch/x86/kernel/macros.s" "-Wa -"]
+           :extraArgs []
+           :pathMappings ,+ccls-path-mappings)
+         :completion
+         (:include
+           (:blacklist
+             ["^/usr/(local/)?include/c\\+\\+/[0-9\\.]+/(bits|tr1|tr2|profile|ext|debug)/"
+               "^/usr/(local/)?include/c\\+\\+/v1/"
+               ]))
+         :index (:initialBlacklist ,+ccls-initial-blacklist :parametersInDeclarations :json-false :trackDependency 1)))
 
-  (after! projectile
-   (add-to-list 'projectile-globally-ignored-directories ".ccls-cache"))
+    (after! projectile
+      (add-to-list 'projectile-globally-ignored-directories ".ccls-cache"))
 
-  (evil-set-initial-state 'ccls-tree-mode 'emacs)
-  )
+  (evil-set-initial-state 'ccls-tree-mode 'emacs))
 
 ;; modern c++ highlight
 (use-package! modern-cpp-font-lock
