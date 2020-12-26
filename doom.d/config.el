a946251cc915124908041a43f3467f6aef3c570d
Xin <xzhang99@binghamton.edu>
2020-12-25 20:56:49 -0500

    Clean and update config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,26 +1,11 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; they are implemented.
+;;  `load!' loading external *.el files
+;;  `use-package!' for configuring packages
+;;  `after!' for running code after a package has loaded
+;;  `add-load-path!'
+;;  `require' or `use-package'.
+;;  `map!' for binding new keys
 
 (load! "+looking")
 (load! "+bindings")
@@ -28,8 +13,15 @@
 
 (setq doom-scratch-buffer-major-mode 'emacs-lisp-mode)
 
-;; Using binding 'K'
-(set-lookup-handlers! 'emacs-lisp-mode :documentation #'helpful-at-point)
+;; use-package!
+;; :commands - load package after commands
+;; :hook - load till hook triggered
+;; :after - load after feature
+;; :init - run immediately
+;; :conifg
+;;
+;; after! - work on feature or lib inovke
+;; add-hook! - work on mode hook trigger
 
 (use-package! avy
   :commands (avy-goto-char-timer)
@@ -38,7 +30,6 @@
   (setq avy-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l ?q ?w ?e ?r ?u ?i ?o ?p))
   )
 
-;; shell
 (after! eshell
   (defun eshell/l (&rest args) (eshell/ls "-l" args))
   (defun eshell/e (file) (find-file file))
@@ -68,41 +59,14 @@
   (add-hook 'eshell-first-time-mode-hook #'+my/eshell-init-keymap)
   )
 
-;; comment
 (use-package! evil-nerd-commenter
-  :commands (evilnc-comment-or-uncomment-lines)
-  )
+  :commands (evilnc-comment-or-uncomment-lines))
 
-;; inc-search
 (setq isearch-lax-whitespace t)
 (setq search-whitespace-regexp ".*")
+
 (define-key isearch-mode-map (kbd "DEL") #'isearch-del-char)
-(defadvice isearch-search (after isearch-no-fail activate)
-  (unless isearch-success
-    (ad-disable-advice 'isearch-search 'after 'isearch-no-fail)
-    (ad-activate 'isearch-search)
-    (isearch-repeat (if isearch-forward 'forward))
-    (ad-enable-advice 'isearch-search 'after 'isearch-no-fail)
-    (ad-activate 'isearch-search)))
 
-(use-package! link-hint
-  :commands link-hint-open-link link-hint-open-all-links)
-
-(use-package! smartparens
-  :config
-  (setq sp-autoinsert-pair t
-        sp-autodelete-pair t
-        sp-escape-quotes-after-insert t)
-  (setq-default sp-autoskip-closing-pair t)
-  )
-
-(set-popup-rules! '(
-  ("^\\*helpful" :size 0.4)
-  ("^\\*info.*" :size 80 :size right)
-  ("^\\*Man.*" :size 80 :side right)
-  ))
-
-;; search engine
 (use-package! ivy
   :config
   (setq
@@ -112,69 +76,79 @@
    ivy-re-builders-alist '((swiper . ivy--regex-plus)
                            (swiper-isearch . ivy--regex-plus)
                            (counsel-rg . ivy--regex-plus)
-                           (t . ivy--regex-plus))
-   )
-  )
+                           (t . ivy--regex-plus))))
 
 (after! flycheck
-  ;; (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  ;;(setq flycheck-check-syntax-automatically '(save mode-enabled))
   (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
-  (global-flycheck-mode -1)
-  )
-
-(after! flymake-proc
-  ;; disable flymake-proc
-  (setq-default flymake-diagnostic-functions nil)
-  )
-(defvar flymake-posframe-delay 0.5)
-(defvar flymake-posframe-buffer "*flymake-posframe*")
-(defvar flymake-posframe--last-diag nil)
-(defvar flymake-posframe--timer nil)
+  (global-flycheck-mode -1))
 
 (setq whitespace-style (quote (trailing space)))
 
-(use-package! company-box
-  :hook (company-mode . company-box-mode))
-(which-func-mode t)
-(lsp-treemacs-sync-mode 1)
+(use-package! treemacs
+  :commands (treemacs)
+  :config
+  (setq treemacs-width 70)
+  )
 
-;; ((use-package! dap-mode
-;;    add-hook 'dap-stopped-hook (lambda (arg) (call-interactively #'dap-hydra)))
-;;   )
-;; (use-package! awesome-tab
-;;   :config
-;;   (awesome-tab-mode t)
-;;   ;; show index
-;;   (setq
-;;      awesome-tab-show-tab-index t
-;;         )
-;;   )
+(use-package! symbol-overlay
+  :commands (symbol-overlay-put))
 
-;; (after! git-link
-;;   (defun git-link-llvm (hostname dirname filename branch commit start end)
-;;       (format "https://github.com/llvm-mirror/%s/tree/%s/%s"
-;;               (file-name-base dirname)
-;;               (or branch commit)
-;;               (concat filename
-;;                       (when start
-;;                         (concat "#"
-;;                                 (if end
-;;                                     (format "L%s-%s" start end)
-;;                                   (format "L%s" start)))))))
-;;   (defun git-link-musl (hostname dirname filename branch commit start end)
-;;       (format "http://git.musl-libc.org/cgit/%s/tree/%s%s%s"
-;;               (file-name-base dirname)
-;;               filename
-;;               (if branch "" (format "?id=%s" commit))
-;;               (if start (concat "#" (format "n%s" start)) "")))
-;;   (defun git-link-sourceware (hostname dirname filename branch commit start end)
-;;     (format "https://sourceware.org/git/?p=%s.git;a=blob;hb=%s;f=%s"
-;;             (file-name-base dirname)
-;;             commit
-;;             (concat filename
-;;                     (when start
-;;                       (concat "#" (format "l%s" start))))))
-;;   (add-to-list 'git-link-remote-alist '("git.llvm.org" git-link-llvm))
-;;   (add-to-list 'git-link-remote-alist '("git.musl-libc.org" git-link-musl))
-;;   (add-to-list 'git-link-remote-alist '("sourceware.org" git-link-sourceware))
-;;   )
+(setq which-key-idle-delay 0)
+
+(after! company
+  (setq company-minimum-prefix-length 2
+        company-quickhelp-delay nil
+        company-show-numbers t
+        company-global-modes '(not comint-mode erc-mode message-mode help-mode gud-mode)
+        company-idle-delay 0.0
+        ))
+
+;; sugestions from lsp-mode
+;; set 100mb
+(setq gc-cons-threshold 200000000)
+(setq read-process-output-max (* 2048 1024)) ;; 2mb
+;; (setq lsp-prefer-capf t)
+(setq lsp-idle-delay 0.500)
+
+(use-package! lsp-mode
+  :commands lsp
+  )
+
+;; Use clangd as the cc lsp backend
+;; (after! lsp-clients
+;;   (remhash 'clangd lsp-clients)
+(use-package! lsp-ui
+  :load-path "~/Dev/lsp-ui"
+  :commands lsp-ui-mode
+  :config
+  (setq
+    lsp-ui-sideline-enable t
+    lsp-ui-sideline-ignore-duplicate t
+    lsp-ui-doc-header nil
+    lsp-ui-doc-include-signature nil
+    lsp-ui-doc-background (doom-color 'base4)
+    lsp-ui-doc-border (doom-color 'fg)
+
+    lsp-ui-peek-expand-function (lambda (xs) (mapcar #'car xs))
+    )
+
+  (map! :after lsp-ui-peek
+    :map lsp-ui-peek-mode-map
+    "h" #'lsp-ui-peek--select-prev-file
+    "j" #'lsp-ui-peek--select-next
+    "k" #'lsp-ui-peek--select-prev
+    "l" #'lsp-ui-peek--select-next-file
+    )
+  )
+
+;; Using binding 'K'
+;; (set-lookup-handlers! 'emacs-lisp-mode :documentation #'helpful-at-point)
+
+;; (defadvice isearch-search (after isearch-no-fail activate)
+;;   (unless isearch-success
+;;     (ad-disable-advice 'isearch-search 'after 'isearch-no-fail)
+;;     (ad-activate 'isearch-search)
+;;     (isearch-repeat (if isearch-forward 'forward))
+;;     (ad-enable-advice 'isearch-search 'after 'isearch-no-fail)
+;;     (ad-activate 'isearch-search)))
